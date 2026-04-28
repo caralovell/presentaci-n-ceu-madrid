f07b32680a84f9ac6eb808dede7ebe85babb3d38
caralovell <168685711+caralovell@users.noreply.github.com>
2026-04-28 09:01:04 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/presentaci-n-ceu-madrid/',
   server: {
     host: "::",
     port: 8080,
